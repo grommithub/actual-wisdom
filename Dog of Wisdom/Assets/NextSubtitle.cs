5c180b591ee651e3b29bd22098b0d3ea8117e572
Daniel Burt <gromitzy@gmail.com>
2019-05-07 17:04:13 +0200

    made whole movie

--- Dog of Wisdom/Assets/NextSubtitle.cs
+++ Dog of Wisdom/Assets/NextSubtitle.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class NextSubtitle : MonoBehaviour
 {
     public List<string> subtitles = new List<string>();
+    private AudioSource audio;
+    public List<AudioClip> audioclips = new List<AudioClip>();
     public TMP_Text box;
 
     public int subtitleIndex;
+    public int audioindex;
 
     private void Start()
     {
+        audio = GetComponent<AudioSource>();
         box.text = subtitles[subtitleIndex];
     }
-    public void ChangeBox()
+    public void PlayAudio()
+    {
+        audioindex++;
+        audio.PlayOneShot(audioclips[audioindex]);
+    }
+    public void OnlyChangeTextBox()
     {
         subtitleIndex++;
         box.text = subtitles[subtitleIndex];
+        
+    }
+    public void Both()
+    {
+        OnlyChangeTextBox();
+        PlayAudio();
+
     }
 }
